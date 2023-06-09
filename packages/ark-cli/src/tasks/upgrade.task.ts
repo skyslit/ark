b89744c0cd9a9316764587c6ca26925df5a9caa5
horus <horus@skyslit.com>
2023-06-09 15:23:44 +0530

    feat: added toolkit upgrade option

--- packages/ark-cli/src/tasks/upgrade.task.ts
+++ packages/ark-cli/src/tasks/upgrade.task.ts
@@ -2,12 +2,20 @@ import Listr from 'listr';
 import inquirer from 'inquirer';
 import chalk from 'chalk';
 import runCommand from '../utils/run-command';
+import ensureDir from '../utils/ensure-dir';
+import path from 'path';
+import fs from 'fs';
+import fetch from 'node-fetch';
+import tar from 'tar-fs';
+import gunzip from 'gunzip-maybe';
+import rimraf from 'rimraf';
+import ncp from 'ncp';
 
-export default (cwd_?: string) => {
+export default (cwd_?: string, shouldUpgradeToolkit: boolean = false) => {
   const packager: 'npm' | 'yarn' = 'npm';
   const cwd = cwd_ || process.cwd();
 
-  const job = new Listr([
+  const steps: Listr.ListrTask<any>[] = [
     {
       title: 'upgrade @skyslit/ark-core',
       task: () =>
@@ -48,20 +56,146 @@ export default (cwd_?: string) => {
           cwd: cwd,
         }),
     },
-  ]);
+  ];
+
+  if (shouldUpgradeToolkit === true) {
+    steps.push({
+      title: 'upgrade toolkit',
+      skip(ctx) {
+        return shouldUpgradeToolkit === false;
+      },
+      task: () => {
+        return new Listr([
+          {
+            title: 'downloading boilerplate',
+            task: async (ctx) => {
+              const tarFilePath = path.join(cwd, 'temp.tar');
+              const tarExtractFilePath = path.join(cwd, 'temp-ext');
+
+              ensureDir(tarExtractFilePath, true, true);
+
+              await new Promise((resolve, reject) => {
+                const file = fs.createWriteStream(tarFilePath);
+                fetch('https://api.github.com/repos/skyslit/ark-base/tarball')
+                  .then((res) => {
+                    res.body.pipe(file);
+                    res.body.on('error', reject);
+                    file.on('finish', () => {
+                      file.close();
+                      resolve(true);
+                    });
+                  })
+                  .catch(reject);
+              });
+
+              await new Promise<void>((resolve, reject) => {
+                fs.createReadStream(tarFilePath)
+                  .pipe(gunzip())
+                  .pipe(tar.extract(tarExtractFilePath, {}))
+                  .on('error', reject)
+                  .on('finish', () => {
+                    resolve();
+                  });
+              });
+            },
+          },
+          {
+            title: 'copying files',
+            task: async (ctx) => {
+              const tarExtractFilePath = path.join(cwd, 'temp-ext');
+              const dirName = fs
+                .readdirSync(tarExtractFilePath)
+                .find((d) => d.indexOf('skyslit-ark-base') > -1);
+              const sourceBaseDir = path.join(tarExtractFilePath, dirName);
+              const targetDir = path.join(
+                cwd,
+                'src',
+                'modules',
+                'main',
+                'toolkit'
+              );
+
+              ensureDir(sourceBaseDir, false, true);
+              ensureDir(targetDir, false, true);
+
+              await new Promise<void>((resolve, reject) => {
+                ncp(
+                  path.join(sourceBaseDir, 'src', 'modules', 'main', 'toolkit'),
+                  targetDir,
+                  (err) => {
+                    if (err) {
+                      reject(err);
+                    } else {
+                      resolve();
+                    }
+                  }
+                );
+              });
+            },
+          },
+          {
+            title: 'cleaning up',
+            task: async (ctx) => {
+              const tarExtractFilePath = path.join(cwd, 'temp-ext');
+              const tarFilePath = path.join(cwd, 'temp.tar');
+              await new Promise<void>((resolve, reject) => {
+                rimraf(tarExtractFilePath, (err) => {
+                  if (err) {
+                    reject(err);
+                  } else {
+                    resolve();
+                  }
+                });
+              });
+
+              await new Promise<void>((resolve, reject) => {
+                rimraf(tarFilePath, (err) => {
+                  if (err) {
+                    reject(err);
+                  } else {
+                    resolve();
+                  }
+                });
+              });
+            },
+          },
+        ]);
+      },
+    });
+  }
+
+  const job = new Listr(steps);
 
   return Promise.resolve()
     .then(() =>
-      inquirer.prompt([
-        {
-          name: 'confirmation',
-          message: 'Do you want to upgrade to the latest possible version?',
-          type: 'confirm',
-          default: false,
-        },
-      ])
+      inquirer.prompt(
+        [
+          (() => {
+            if (shouldUpgradeToolkit === true) {
+              return {
+                name: 'toolkit_confirmation',
+                message:
+                  '[Imp.] Do you want to upgrade toolkit along with the framework?',
+                type: 'confirm',
+                default: false,
+              };
+            }
+            return undefined;
+          })(),
+          {
+            name: 'confirmation',
+            message: 'Do you want to upgrade to the latest possible version?',
+            type: 'confirm',
+            default: false,
+          },
+        ].filter(Boolean)
+      )
     )
     .then((input) => {
+      if (input.toolkit_confirmation === false) {
+        shouldUpgradeToolkit = false;
+      }
+
       if (input.confirmation === true) {
         return job
           .run({
@@ -69,6 +203,13 @@ export default (cwd_?: string) => {
             ...input,
           })
           .then(() => {
+            if (shouldUpgradeToolkit === true) {
+              console.log(
+                chalk.yellow(
+                  'You may need to manualy install new dependencies used in the latest version of toolkit.'
+                )
+              );
+            }
             console.log(
               chalk.green(
                 'Upgrade success. Please restart any development process to take effect.'
